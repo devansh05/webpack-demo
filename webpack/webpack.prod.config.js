cb54a3176ca137ff60872f130e6c0e57f36d880e
devansh05 <devansh2393@gmail.com>
2025-12-13 01:25:26 +0530

    minifying css modules

--- webpack/webpack.prod.config.js
+++ webpack/webpack.prod.config.js
@@ -1,11 +1,25 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.config.js");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
   output: {
     filename: "js/[name].[contenthash:12].js",
+  },
+  optimization: {
+    minimize: true,
+    minimizer: ["...", new CssMinimizerPlugin({
+      minimizerOptions: {
+        preset: [
+          "default",
+          {
+            discardComments: { removeAll: true },
+          },
+        ],
+      },
+    })],
   },
   module: {
     rules: [
